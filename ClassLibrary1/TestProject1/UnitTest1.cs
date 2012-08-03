a619ea074ed029b98b1a7d29f7f3bd6c277b04c6
Will Osborn <wosborn@tx.rr.com>
2012-08-03 13:18:24 -0500

    Test app added
    MS unit test app for simplicity

---
+++ ClassLibrary1/TestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WillPower.Math.Demo;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                FizzBuzz fb = new FizzBuzz();
+                fb.Dividends.Add(3, "Fizz");
+                fb.Dividends.Add(5, "Buzz");
+                string s = fb.GetOutput(1, 100);
+                if (string.IsNullOrEmpty(s)) throw new ApplicationException();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+    }
+}
